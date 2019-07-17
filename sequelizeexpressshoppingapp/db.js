4ad443acbfdd9df00f271cfcfd1e233faf4b53cb
neeraj <neeeraj2599@gmail.com>
2019-07-17 19:49:25 +0530

    shopping app

---
+++ sequelizeexpressshoppingapp/db.js
@@ -0,0 +1,31 @@
+const sequelize = require('sequelize');
+const db = new sequelize('shopdb','shopper','shoppass',{
+    host:'localhost',
+    dialect:'mysql',
+    pool:{
+        min:0,
+        max:5
+    }
+})
+const User = db.define('users',{
+    id:{type:sequelize.INTEGER,autoIncrement:true,primaryKey :true},
+    name:{
+        type:sequelize.STRING,allowNULL:false
+    }
+})
+const Product = db.define('products',{
+    id:{type:sequelize.INTEGER,autoIncrement:true,primaryKey :true},
+    name:{
+        type:sequelize.STRING,allowNULL:false
+    },
+    manufacturer:{
+        type:sequelize.STRING
+    },
+    price:{
+        type:sequelize.FLOAT,allowNULL:false,defaultvalue:0.0
+    }
+})
+db.sync()
+     .then(()=>console.log("database synced"))
+     .catch((err)=>console.error("DATABASE NOT SYNCED"))
+exports=module.exports={User,Product}
